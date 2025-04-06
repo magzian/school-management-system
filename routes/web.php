be8faa5c280c5ba5e66cc4702be7b301e73fba0a
Ian Magonya <ianbenard2002@gmail.com>
2025-04-06 12:07:41 +0300

    admin layout added

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/admin', function () {
+    return Inertia::render('Admin/Dashboard');
+});
+
 require __DIR__.'/auth.php';
